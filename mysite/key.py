c4276aa54d8932cc909d97ffb85f863eccf75de2
Ariel Huang <jinyi@umail.ucsb.edu>
2017-11-18 12:14:45 -0800

    My Django Girls app, first commit

---
+++ mysite/key.py
@@ -0,0 +1,1 @@
+key = 'h-l_+e+^q*k1ny2y7u&g55@9-=f_(x+7($lc5msu8vfm6$^*qf'
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h-l_+e+^q*k1ny2y7u&g55@9-=f_(x+7($lc5msu8vfm6$^*qf'
+SECRET_KEY = key
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
